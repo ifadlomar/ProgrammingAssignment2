5f222a48d00614e300cfc9cd4e5a39a6e60e03fb
ifadlomar <ibrahim_m_a@yahoo.com>
2015-02-21 01:39:30 +0200

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## These two functions one called makeCacheMatrix  for cashing the the value of matrinx 
+##and the other one cacheSolve for calculate the inverse of the matrix
+ 
+## function makeCacheMatrix take a matrix and return alist of cashed value
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
