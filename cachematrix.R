e02581becfef7edc62b13e572f798053a98c357c
ifadlomar <ibrahim_m_a@yahoo.com>
2015-02-21 02:07:44 +0200

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,36 @@
 ## These two functions one called makeCacheMatrix  for cashing the the value of matrinx 
 ##and the other one cacheSolve for calculate the inverse of the matrix
- 
+
 ## function makeCacheMatrix take a matrix and return alist of cashed value
 
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL  
+  }
+  get <- function() x 
+  setinvmatrix <- function(invmatrix) m <<- invmatrix
+  getinvmatrix <- function() m
+  list(set = set, get = get,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
+  
+}
+
+
+
+## function cacheSolve calculate the inverse of the input matrix
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvmatrix ()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvmatrix(m)
+  m
+}
